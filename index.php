51631712aad922dd4d6bc584bba78121d72f3d04
Narresx <92748159+Narresx@users.noreply.github.com>
2022-02-28 16:08:05 +0100

    add header

--- index.php
+++ index.php
@@ -115,16 +115,33 @@ $paragraphs = [
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" 
     rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" 
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>Google FAQ (demo)</title>
+    <style> 
+    img{
+        width: 100px;
+        height: auto;
+        overflow: hidden;
+    }
+
+    header a{
+        text-decoration: none;
+    }
+    </style>
 </head>
 <body>
-    
-    <?php foreach($paragraphs as $paragraph) {?>
+    <header class="row justify-content-between">
+        <div class="col-3"><img src="https://www.keypersonofinfluence.com/wp/wp-content/uploads/2015/08/entrevo-keypersonofinfluence-get-discovered-on-google.jpg" alt="Google"><strong><a href="#" class="link-secondary">Privacy e termini</a></strong></div>
+        <div class="col-3 text-end"><i class="fa-thin fa-grid"></i></div>
+    </header>
+    <section class="container m-auto">
+     <?php foreach($paragraphs as $paragraph) {?>
            
         <h2><?php echo $paragraph['title']?></h2>
         <p><?php echo $paragraph['body']?></p>
 
-    <?php } ?>
-
+     <?php } ?>
+    </section>
 </body>
 </html>
